3735db3224baa561c29c9047c05aa7f2d52fe20a
dynecore <aira@dynecore.com>
2024-03-08 23:01:08 -0300

    Prueba con express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-ll('Hello World!')
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    ll('--Pagina / cargada --')
+    res.send('<h1>Bienvenido a Dynecore!</h1> <br /> Sistemas en linea!')
+})
+
+app.listen(PORT)
+ll('Servidor iniciado en puerto', PORT)
